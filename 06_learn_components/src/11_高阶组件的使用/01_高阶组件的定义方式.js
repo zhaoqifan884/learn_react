b48e1c745974a08d6dfde569df50d25f8d57b2ce
zhaoqifan884 <zqf8840580>
2020-07-03 22:33:07 +0800

    高阶组件应用场景

--- 06_learn_components/src/11_高阶组件的使用/01_高阶组件的定义方式.js
+++ 06_learn_components/src/11_高阶组件的使用/01_高阶组件的定义方式.js
@@ -4,20 +4,32 @@ class App extends PureComponent {
     render() {
         return (
             <div>
-               Aoo
+               Aoo: {this.props.name}
             </div>
         );
     }
 }
 
-//高阶组件    接收一个组件作为参数，并返回另外一个组件
-function enhanceComponent(WrapperComponent) {
-    return class NewComponent extends PureComponent{
+//高阶组件     参数为组件，返回值为新组件的函数
+// 接收一个组件作为参数，并返回另外一个组件
+function enhanceComponent(WrappedComponent) {
+    class NewComponent extends PureComponent {
         render() {
-            return <WrapperComponent/>
+            return <WrappedComponent {...this.props}/>
         }
     }
+
+    NewComponent.displayName = "Kobe";
+    return NewComponent;
 }
+
+/*function enhanceComponent2(WrapperComponent) {
+    function NewComponent(props) {
+        return <WrapperComponent {...props}/>
+    }
+    NewComponent.displayName = "Lion";
+    return NewComponent
+}*/
 
 const EnhanceComponent = enhanceComponent(App);
 
--- 06_learn_components/src/index.js
+++ 06_learn_components/src/index.js
@@ -35,6 +35,10 @@ import ReactDOM from "react-dom"
 // import App from "./10_受控和非受控组件/03_受控组件-select的使用";
 // import App from "./10_受控和非受控组件/04_受控组件-多输入";
 // import App from "./10_受控和非受控组件/05_非受控组件的使用";
-import App from "./11_高阶组件的使用/01_高阶组件的定义方式";
+// import App from "./11_高阶组件的使用/01_高阶组件的定义方式";
+// import App from "./11_高阶组件的使用/02_高阶组件的应用_增强props";
+// import App from "./11_高阶组件的使用/03_高阶组件的应用_增强props_默认";
+import App from "./11_高阶组件的使用/04_高阶组件的应用_增强props_改进";
 
 ReactDOM.render(<App/>, document.getElementById("root"));
+// ReactDOM.render(<App name={'why'}/>, document.getElementById("root"));//11-01
