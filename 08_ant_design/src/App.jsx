48204139559a809fa7e400fd709118574841befa
zhaoqifan884 <584123809@qq.com>
2020-07-08 16:22:26 +0800

    antDesign

---
+++ 08_ant_design/src/App.jsx
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react';
+
+class App extends PureComponent {
+    render() {
+        return (
+            <div>
+                App
+            </div>
+        );
+    }
+}
+
+
+export default App;
