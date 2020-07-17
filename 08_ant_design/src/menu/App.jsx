c6a2405ec207238b2a5d383416ab0777dbfe1371
zhaoqifan884 <584123809@qq.com>
2020-07-17 10:21:22 +0800

    导航栏

--- 08_ant_design/src/menu/App.jsx
+++ 08_ant_design/src/menu/App.jsx
@@ -1,43 +1,58 @@
 import React, {PureComponent} from 'react';
-import {Menu} from "antd";
+import {Menu, Switch} from "antd";
 import { MailOutlined} from "@ant-design/icons";
 
 class App extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            theme: 'dark',
+            current: '1'
+        }
+    }
     render() {
         const {SubMenu} = Menu;
+        const {theme, current} = this.state;
         return (
-            <Menu
-                onClick={this.handleClick}
-                style={{ width: 256 }}
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                mode="inline"
-            >
-                <SubMenu
-                    key="sub1"
-                    title={
-                        <span>
-              <MailOutlined />
-              <span>Navigation One</span>
-            </span>
-                    }
-                >
-                    <Menu.ItemGroup key="g1" title="Item 1">
+            <>
+                <Switch
+                    checked={theme === 'dark'}
+                    onChange={value => this.changeTheme(value)}
+                    checkedChildren="Dark"
+                    unCheckedChildren="Light"/>
+                <br />
+                <br />
+                <Menu
+                    theme={theme}
+                    onClick={event => this.handleClick(event)}
+                    style={{ width: 256 }}
+                    defaultOpenKeys={['sub1']}
+                    selectedKeys={[current]}
+                    mode="inline">
+                    <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
                         <Menu.Item key="1">Option 1</Menu.Item>
                         <Menu.Item key="2">Option 2</Menu.Item>
-                    </Menu.ItemGroup>
-                    <Menu.ItemGroup key="g2" title="Item 2">
                         <Menu.Item key="3">Option 3</Menu.Item>
                         <Menu.Item key="4">Option 4</Menu.Item>
-                    </Menu.ItemGroup>
-                </SubMenu>
-            </Menu>
+                    </SubMenu>
+                </Menu>
+            </>
         );
     }
 
-    handleClick = e => {
-        console.log('click ', e);
-    };
+    handleClick(event) {
+        // console.log(event.key)
+        this.setState({
+            current: event.key
+        })
+    }
+
+    changeTheme(value) {
+        this.setState({
+            theme: value? 'dark' : 'light'
+        })
+    }
 }
 
 
