d9b6deb0925725ec5cd077924c85ef15428c06c5
zhaoqifan <584123809@qq.com>
2020-07-18 10:11:50 +0800

    国际化

--- 08_ant_design/src/index.js
+++ 08_ant_design/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'antd/dist/antd.css'
+//国际化
+import 'moment/locale/zh-cn'
 import 'antd/dist/antd.less'
 
 // import App from './classNames/App';
