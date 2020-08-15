c5a5cb800f5636cc94c77e02b3d76e823c702951
zhaoqifan884 <zqf8840580>
2020-08-15 21:56:15 +0800

    reducer

--- 14_redux-reducer/src/store/reducer.js
+++ 14_redux-reducer/src/store/reducer.js
@@ -3,12 +3,19 @@ import { reducer as homeReducer } from './home';
 
 // import { combineReducers } from 'redux';
 
+/**
+ *
+ * @param state 传递一个空对象，相当于state中的counterInfo，homeInfo为空对象
+ * @param action
+ * @returns {{counterInfo: ({counter: *}|{counter: number}), homeInfo: ({recommends: [], banners: ([]|*[])}|{recommends: ([]|*[]), banners: []}|{recommends: [], banners: []})}}
+ */
 function reducer(state = {}, action) {
   return {
     counterInfo: counterReducer(state.counterInfo, action),
     homeInfo: homeReducer(state.homeInfo, action)
   }
 }
+
 
 //reducer应该是一个什么类型? function
 // const reducer = combineReducers({
