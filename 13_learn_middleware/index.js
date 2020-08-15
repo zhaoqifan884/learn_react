ad3198db1d55b0fda681332f2559b73be8ff120b
zhaoqifan <584123809@qq.com>
2020-08-15 17:49:16 +0800

    redux-middleware

--- 13_learn_middleware/index.js
+++ 13_learn_middleware/index.js
@@ -33,11 +33,78 @@ import {
 //   console.log("dispatch后---new state:", store.getState());
 // }
 // store.dispatch = dispatchAndLogging;
-
+//
 // store.dispatch(addAction(10));
 // store.dispatch(addAction(5));
 
-// 4.将之前的操作进行封装
+// 4.1将之前的操作进行封装
+// 封装patchLogging的代码
+// function patchLogging(store) {
+//   const next = store.dispatch;
+//   function dispatchAndLogging(action) {
+//     console.log("dispatch前---dispatching action:", action);
+//     next(action);
+//     console.log("dispatch后---new state:", store.getState());
+//   }
+//   store.dispatch = dispatchAndLogging;
+//
+// }
+// patchLogging(store);
+// store.dispatch(addAction(10));
+// store.dispatch(addAction(5));
+
+//4.2 封装patchThunk的功能
+// 封装patchLogging的代码
+// function patchLogging(store) {
+//   const next = store.dispatch;
+//   function dispatchAndLogging(action) {
+//     console.log("dispatch前---dispatching action:", action);
+//     next(action);
+//     console.log("dispatch后---new state:", store.getState());
+//   }
+//   store.dispatch = dispatchAndLogging;
+// }
+//
+// //封装patchThunk的功能
+// function patchThunk(store) {
+//   const next = store.dispatch;
+//
+//   function dispatchAndThunk(action) {
+//     //Thunk不仅能传递一个对象，也能传递一个函数
+//     if (typeof action === "function") {
+//       //函数
+//       //ps: 函数()： 执行函数
+//       action(next, store.getState)
+//     } else {
+//       //对象
+//       next(action);
+//     }
+//   }
+//   store.dispatch = dispatchAndThunk;
+// }
+// patchLogging(store);
+// patchThunk(store);
+// //传方法
+// // store.dispatch(addAction(10));
+// // store.dispatch(addAction(5));
+//
+// //调取函数时：
+// function foo(dispatch, getState) {
+//   dispatch(subAction(10))
+// }
+// store.dispatch(foo);
+
+// 5.封装applyMiddleware      （真实开发）
+function applyMiddlewares(...middlewares) {
+  //前拷贝   把之前的变量展开赋值给newMiddleware，怕对newMiddleware做修改
+  const newMiddleware = [...middlewares];
+  middlewares.forEach(middleware => {
+    //middleware 实际就是applyMiddlewares的两个参数
+    store.dispatch = middleware(store);
+  })
+}
+applyMiddlewares(patchLogging, patchThunk);
+
 // 封装patchLogging的代码
 function patchLogging(store) {
   const next = store.dispatch;
@@ -46,46 +113,25 @@ function patchLogging(store) {
     next(action);
     console.log("dispatch后---new state:", store.getState());
   }
-  // store.dispatch = dispatchAndLogging;
-
   return dispatchAndLogging;
 }
 
 // 封装patchThunk的功能
 function patchThunk(store) {
   const next = store.dispatch;
-
   function dispatchAndThunk(action) {
     if (typeof action === "function") {
-      action(store.dispatch, store.getState)
+      //函数
+      action(next, store.getState)
     } else {
-      next(action);
+      next(action)
     }
   }
-
-  // store.dispatch = dispatchAndThunk;
   return dispatchAndThunk;
 }
 
 patchLogging(store);
 patchThunk(store);
 
-// store.dispatch(addAction(10));
-// store.dispatch(addAction(5));
 
-// function foo(dispatch, getState) {
-//   dispatch(subAction(10));
-// }
-
-// store.dispatch(foo);
-
-// 5.封装applyMiddleware
-function applyMiddlewares(...middlewares) {
-  // const newMiddleware = [...middlewares];
-  middlewares.forEach(middleware => {
-    store.dispatch = middleware(store);
-  })
-}
-
-applyMiddlewares(patchLogging, patchThunk);
 
